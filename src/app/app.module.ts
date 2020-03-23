6cf5da922a5c994d77af1a03d095bf5f99382d0a
Andrey Marunyak <marunyak.andrey@gmail.com>
2020-03-24 00:32:58 +0200

    Created project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeePageComponent } from './pages/employee-page/employee-page.component';
+import { EmployesPageComponent } from './pages/employes-page/employes-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeePageComponent,
+    EmployesPageComponent
   ],
   imports: [
     BrowserModule,
